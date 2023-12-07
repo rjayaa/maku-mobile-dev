22ff3f91b8e3d4c0e40bf4d87c6959a364391bb2
rjayaa <rjaya.lelana03@gmail.com>
2023-12-07 12:14:47 +0700

    init project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.project.maku_mobile_based"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.project.maku_mobile_based"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/project/maku_mobile_based/controller/LoginActivityController.java
+++ app/src/main/java/com/project/maku_mobile_based/controller/LoginActivityController.java
@@ -1,14 +1,16 @@
-package com.project.maku_mobile_based;
+package com.project.maku_mobile_based.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.project.maku_mobile_based.R;
+
+public class LoginActivityController extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_view);
     }
 }
