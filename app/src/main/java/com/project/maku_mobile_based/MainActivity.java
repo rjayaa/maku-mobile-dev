30de977b87e3512e0dcf3db0298401b11ef41fe1
Fira <zaafirasf@gmail.com>
2023-12-08 16:30:58 +0700

    change first page to MainAcitvity

--- app/src/main/java/com/project/maku_mobile_based/MainActivity.java
+++ app/src/main/java/com/project/maku_mobile_based/MainActivity.java
@@ -2,13 +2,37 @@ package com.project.maku_mobile_based;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button SignInButton, SignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton = findViewById(R.id.SignInButton);
+        SignUpButton = findViewById(R.id.RegisterButton);
+
+        SignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
