17d9945a0d0bd8feda6025bdc53432c7c255bbc2
fdaniall <fakhridanial29@gmail.com>
2024-01-05 23:37:34 +0700

    pushhh

--- app/src/main/java/com/project/maku_mobile_based/Activity/CheckoutActivity.java
+++ app/src/main/java/com/project/maku_mobile_based/Activity/CheckoutActivity.java
@@ -11,5 +11,8 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkout_view);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
     }
 }
