36c24358031facf27817ec22a3c78ba3e9d1f312
fdaniall <fakhridanial29@gmail.com>
2024-01-06 00:28:48 +0700

    siuiuuiui

---
+++ app/src/main/java/com/project/maku_mobile_based/Activity/HistoryOrdersActivity.java
@@ -0,0 +1,16 @@
+package com.project.maku_mobile_based.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.maku_mobile_based.R;
+
+public class HistoryOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
